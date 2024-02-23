1dac68920374257df346869d3b17ddf8f23fc455
Teunemans <tevanbostelen@gmail.com>
2024-02-23 17:28:39 +0100

    mdn + prime

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -5,22 +5,23 @@ function getComputerChoice() {
 
 function playRound(computerSelection, playerInput) {
     let playerSelection = playerInput.toLowerCase()
-    if (computerSelection == playerSelection) {
-        return "draw"
+    if (computerSelection === playerSelection) {
+        return "Draw, you both chose " + playerSelection + "!"
     } else if (computerSelection === "rock" && playerSelection === "scissors") {
-        return "You lost!, " + computerSelection + " beats " + playerSelection
+        return "You lost! " + computerSelection + " beats " + playerSelection
     } else if (computerSelection === "scissors" && playerSelection === "paper") {
-        return "You lost!, " + computerSelection + " beats " + playerSelection
+        return "You lost! " + computerSelection + " beats " + playerSelection
     } else if (computerSelection === "paper" && playerSelection === "rock") {
-        return "You lost!, " + computerSelection + " beats " + playerSelection
+        return "You lost! " + computerSelection + " beats " + playerSelection
     } else {
-        return "You won!, " + playerSelection + " beats " + computerSelection
+        return "You won! " + playerSelection + " beats " + computerSelection
     }
 }
 
 function playGame() {
     for (let i = 0; i < 5; i++) {
-        playRound(getComputerChoice(), prompt("Please enter your choice: "))
+        let playerInput = prompt("Please enter your choice: ")
+        console.log(playRound(getComputerChoice(), playerInput))
     }
 }
 
