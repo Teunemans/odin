0663befdf40b241926de1803866c1ef918fbee87
Teunemans <tevanbostelen@gmail.com>
2024-02-24 21:21:57 +0100

    01 - 03 done

---
+++ RockPaperScissors/README.md
@@ -0,0 +1,3 @@
+# RockPaperScissors
+A little js script that lets you play the popular childrens game!
+Based on the Odin Project.
---
+++ foo.js/script.js
@@ -0,0 +1,10 @@
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+for (const cat of cats) {
+  console.log(cat);
+}
+
+// Websockets
+// HTTP
+// Datastructures
+// Algrothims
