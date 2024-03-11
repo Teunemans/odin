1a232e2829f35c66a930d10b9996c1c1ce1a9d77
teunemans <tevanbostelen@gmail.com>
2024-03-11 10:47:59 +0100

    Stylesheet not linking

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,7 +1,8 @@
 const container = document.getElementById("container")
 
 // Make the grid
-for (let i = 0; i < 256; i++) {
-	document.createElement("div")
-	
+for (let i = 0; i <= 256; i++) {
+	const div1 = document.createElement("div")
+	div1.textContent = i
+	container.appendChild(div1)
 }
