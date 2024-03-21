4621a2ad55d9c5b23aac01c33308e428038d4911
Teunemans <tevanbostelen@gmail.com>
2024-03-21 14:45:11 +0100

    hover effect working but not in reverse

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -5,6 +5,15 @@ const container = document.getElementById("container")
 for (let i = 0; i < 256; i++) {
 	const gridDiv = document.createElement("div");
 	gridDiv.classList.add("grid");
-	gridDiv.setAttribute("style", "width: 20px; height: 20px; border: red 2px solid")
+	gridDiv.setAttribute("style",
+		"width: 20px; height: 20px; border: red 2px solid;")
+
+	gridDiv.setAttribute("style", "grid:hover {background")
+	gridDiv.addEventListener("mouseover", function (element) {
+		gridDiv.style.backgroundColor = "green";
+	} )
+	gridDiv.addEventListener("mouseleave", function (element) {
+		gridDiv.style.backgroundcolor = "white";
+	})
 	container.appendChild(gridDiv);
 }
