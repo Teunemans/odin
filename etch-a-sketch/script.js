e99986c5ac8e1a674768e0bcd7bca1bde27cd990
Teunemans <tevanbostelen@gmail.com>
2024-03-23 21:54:37 +0100

    logging

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,19 +1,25 @@
 const container = document.getElementById("container")
 
-// Make the divs for the grid
 
+// hover thing
+function hover(element, enter, leave){
+	element.addEventListener('mouseenter', enter)
+	element.addEventListener('mouseleave', leave)
+}
+
+// Make the divs for the grid
 for (let i = 0; i < 256; i++) {
 	const gridDiv = document.createElement("div");
 	gridDiv.classList.add("grid");
 	gridDiv.setAttribute("style",
 		"width: 20px; height: 20px; border: red 2px solid;")
 
-	gridDiv.setAttribute("style", "grid:hover {background")
-	gridDiv.addEventListener("mouseover", function (element) {
+	gridDiv.addEventListener("mouseenter", function (element) {
 		gridDiv.style.backgroundColor = "green";
 	} )
-	gridDiv.addEventListener("mouseleave", function (element) {
+	gridDiv.addEventListener("mouseout", function (element) {
 		gridDiv.style.backgroundcolor = "white";
+		console.log(i)
 	})
 	container.appendChild(gridDiv);
 }
