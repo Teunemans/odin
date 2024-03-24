4068f5e831993e289ebb98a7b23874129fd4926e
Teunemans <tevanbostelen@gmail.com>
2024-03-24 19:34:52 +0100

    Grid can be redrawn with button

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,25 +1,34 @@
 const container = document.getElementById("container")
 
-
-// hover thing
-function hover(element, enter, leave){
-	element.addEventListener('mouseenter', enter)
-	element.addEventListener('mouseleave', leave)
-}
-
+function makeGrid() {
+	// remove previous grid items from the thing
+	while (container.hasChildNodes()) {
+		container.removeChild(container.lastChild);
+	}
 // Make the divs for the grid
-for (let i = 0; i < 256; i++) {
-	const gridDiv = document.createElement("div");
-	gridDiv.classList.add("grid");
-	gridDiv.setAttribute("style",
-		"width: 20px; height: 20px; border: red 2px solid;")
 
-	gridDiv.addEventListener("mouseenter", function (element) {
-		gridDiv.style.backgroundColor = "green";
-	} )
-	gridDiv.addEventListener("mouseout", function (element) {
-		gridDiv.style.backgroundcolor = "white";
-		console.log(i)
-	})
-	container.appendChild(gridDiv);
+	x = parseInt(prompt("What size do you want your grid to be?"))
+	container.style.gridTemplateColumns = "repeat("+ x +", 20px)"
+	container.style.gridTemplateRows = "repeat("+ x +", 20px)"
+	for (let i = 0; i < (x * x); i++) {
+		const gridDiv = document.createElement("div");
+		container.style.gridTemplateColumns = "repeat("+ x +", 20px)"
+		container.style.gridTemplateRows = "repeat("+ x +", 20px)"
+		gridDiv.classList.add("grid");
+		gridDiv.setAttribute("style",
+			"width: 20px; height: 20px; border: red 2px solid;")
+
+		gridDiv.addEventListener("mouseenter", function (element) {
+			gridDiv.style.backgroundColor = "green";
+		} )
+		gridDiv.addEventListener("mouseout", function (element) {
+			gridDiv.style.backgroundColor = "";
+			console.log(i)
+		})
+		container.appendChild(gridDiv);
+	}
+
 }
+
+const gridButton  = document.getElementById("gridButton");
+gridButton.addEventListener("click", makeGrid())
